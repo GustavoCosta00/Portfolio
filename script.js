ceb413cecd1e84e1d3bb229efe76fabeeab50b30
Gustavo Costa <gustavocosta.franco.almeida@gmail.com>
2025-04-11 22:40:55 -0300

    protfólio

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop - 80, // ajuste para header fixo
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+  
